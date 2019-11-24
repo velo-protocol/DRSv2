34cc3ae099ef885d0cbc36961da8a414b387a28d
Adirut Nithilerdviwat <adirut.wn@hotmail.com>
2019-11-24 11:36:41 +0700

    restructure folders

---
+++ truffle-config.js
@@ -0,0 +1,31 @@
+const PrivateKeyProvider = require("truffle-privatekey-provider");
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+    testnet: {
+      provider: () => new PrivateKeyProvider(process.env.SCC_PK, process.env.EVRYNET_HOST),
+      network_id: 15,
+      gasPrice: 1000000000,
+      production: false
+    }
+  },
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+  compilers: {
+    solc: {
+      settings: {
+       optimizer: {
+         enabled: true,
+         runs: 200
+       },
+      }
+    }
+  }
+}
