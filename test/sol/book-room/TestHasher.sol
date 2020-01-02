64d83002763955f0e5547c3fc6096ab94db8f3c9
WasinTaw <dreamwstt@gmail.com>
2020-01-02 15:14:52 +0700

    Fix unit test

--- test/sol/book-room/TestHasher.sol
+++ test/sol/book-room/TestHasher.sol
@@ -11,59 +11,33 @@ contract TestHasher {
     }
 
     function testLinkId() public {
-        bytes4 selector = hasher.linkId.selector;
-        bytes32 collateralAssetCode = 'VELO';
-        bytes32 peggedCurrency = 'THB';
-        bytes memory data = abi.encodeWithSelector(selector, collateralAssetCode, peggedCurrency);
-        (bool result,) = address(hasher).call(data);
+        bytes32 expected = 0x4cb01440ae0bc757f71beb1faa542cd6a1aba2477a2b38aa35a7eee3283535e9;
+        bytes32 collateralAssetCode = "VELO";
+        bytes32 peggedCurrency = "vTHB";
+        bytes32 result = hasher.linkId(collateralAssetCode, peggedCurrency);
 
-        Assert.isTrue(result, "hasher.linkId() should be true");
-    }
-
-    function testLinkIdWithError() public {
-        bytes4 selector = hasher.linkId.selector;
-        bytes32 collateralAssetCode = bytes32('VELO');
-        bytes memory data = abi.encodeWithSelector(selector, collateralAssetCode);
-        (bool result,) = address(hasher).call(data);
-
-        Assert.isFalse(result, "hasher.linkId() should throw an error");
+        Assert.equal(result,expected, "hasher.linkId() result should be 0x4cb01440ae0bc757f71beb1faa542cd6a1aba2477a2b38aa35a7eee3283535e9");
     }
 
     function testHasherCallFunctionLockedReserveId() public {
+        bytes32 expected = 0x4afd1c8d71908c0a77df9b5973780be5f71f53c6a2ceb7f102746ef30eb5c9df;
         bytes4 selector = hasher.lockedReserveId.selector;
-        address from = address(1);
+        address from = 0x51800B4E5acD84BcA3b1a2be1d07158E35851Cb3;
         bytes32 collateralAssetCode = "VELO";
         uint256 collateralAmount = 100;
         uint blockNumber = 1;
-        bytes memory data = abi.encodeWithSelector(selector, from, collateralAssetCode, collateralAmount, blockNumber);
-        (bool result,) = address(hasher).call(data);
+        bytes32 result = hasher.lockedReserveId(from, collateralAssetCode, collateralAmount, blockNumber);
 
-        Assert.equal(result, true, "Hasher lockedReserveId() should be true");
-    }
-
-    function testHasherLockedReserveIdWithErrorNoneParameters() public {
-        bytes4 selector = hasher.lockedReserveId.selector;
-        bytes memory data = abi.encodeWithSelector(selector);
-        (bool result,) = address(hasher).call(data);
-
-        Assert.equal(result, false, "Hasher lockedReserveId() should throw an error");
+        Assert.equal(result,expected, "Hasher lockedReserveId() result should be 0x4afd1c8d71908c0a77df9b5973780be5f71f53c6a2ceb7f102746ef30eb5c9df");
     }
 
     function testStableCreditId() public {
-        bytes4 selector = hasher.stableCredit.selector;
+        bytes32 expected = 0x82d8c2282adba9ac0ab3f1a555a37692290b0598488f7fc4dfc2e2f8991ecbaf;
         address creditOwner = 0xb04aD816e86bFA5515c35Ad02081F71D9E848C88;
-        string memory assetCode = 'vTHB';
-        bytes memory data = abi.encodeWithSelector(selector, creditOwner, assetCode);
-        (bool result,) = address(hasher).call(data);
+        string memory assetCode = "vTHB";
+        bytes32 result = hasher.stableCredit(creditOwner, assetCode);
 
-        Assert.equal(result, true, "Hasher stableCredit() should be true");
+        Assert.equal(result, expected, "Hasher stableCredit() result should be 0x82d8c2282adba9ac0ab3f1a555a37692290b0598488f7fc4dfc2e2f8991ecbaf");
     }
 
-    function testStableCreditIdWithErrorNoneParameters() public {
-        bytes4 selector = hasher.stableCredit.selector;
-        bytes memory data = abi.encodeWithSelector(selector);
-        (bool result,) = address(hasher).call(data);
-
-        Assert.equal(result, false, "Hasher stableCredit() should throw an error");
-    }
 }
