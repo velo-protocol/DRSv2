22f94bf31cf6928d9c1631ea5e0590fbb2afd33b
WasinTaw <dreamwstt@gmail.com>
2020-01-02 14:30:40 +0700

    Auto stash before merge of "feature/add-hash-stavbleCreditId" and "origin/feature/implement-hasher-library"

--- contracts/modules/book-room/Hasher.sol
+++ contracts/modules/book-room/Hasher.sol
@@ -9,4 +9,8 @@ contract Hasher {
     function lockedReserveId(address from, bytes32 collateralAssetCode, uint256 collateralAmount, uint blockNumber) public pure returns (bytes32) {
         return keccak256(abi.encodePacked(from, collateralAssetCode, collateralAmount, blockNumber));
     }
+
+    function stableCredit(address creditOwner, string memory assetCode) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(creditOwner, assetCode));
+    }
 }
