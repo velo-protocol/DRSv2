3cc32ed6eaa7db0a38c27fd8feed0d8163ac35fd
athiwatp <athiwat.p5@gmail.com>
2020-01-06 14:46:37 +0700

    add case 11 - 21 (#15)

--- test/sol/mocks/MockIERC20.sol
+++ test/sol/mocks/MockIERC20.sol
@@ -1,28 +1,27 @@
 pragma solidity ^0.5.0;
 
 contract MockIERC20 {
-    function totalSupply() external view returns (uint256){
+    function totalSupply() external pure returns (uint256){
         return 0;
     }
 
-    function balanceOf(address) external view returns (uint256){
+    function balanceOf(address) external pure returns (uint256){
         return 0;
     }
 
-    function transfer(address, uint256) external returns (bool){
+    function transfer(address, uint256) external pure returns (bool){
         return true;
     }
 
-    function allowance(address, address) external view returns (uint256){
+    function allowance(address, address) external pure returns (uint256){
         return 0;
     }
 
-    function approve(address, uint256) external returns (bool){
+    function approve(address, uint256) external pure returns (bool){
         return true;
     }
 
-    function transferFrom(address, address, uint256) external returns (bool){
+    function transferFrom(address, address, uint256) external pure returns (bool){
         return true;
     }
-
 }
