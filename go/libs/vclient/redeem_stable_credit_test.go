1b6405fe7eea529f10a5c115a2fc3ed7c71f0647
Michael <michael.ran@lightnet.io>
2020-02-05 16:49:14 +0700

    finish up the input unit test, but have not added TestRedeemStableCredit() yet.

--- go/libs/vclient/redeem_stable_credit_test.go
+++ go/libs/vclient/redeem_stable_credit_test.go
@@ -58,52 +58,15 @@ func TestValidateRedeemStableCredit(t *testing.T) {
 	})
 }
 
-//func TestValidateRedeemStableCreditToAbiInput(t *testing.T) {
-//	t.Run("success", func(t *testing.T) {
-//		redeemStableCreditInput := &RedeemStableCreditInput{AssetCode: "vUSD"}
-//		abiInput := redeemStableCreditInput.ToAbiInput()
-//
-//		assert.NotNil(t, abiInput)
-//	})
-//}
+func TestValidateRedeemStableCreditToAbiInput(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		redeemStableCreditInput := &RedeemStableCreditInput{RedeemAmount: "100", AssetCode: "vUSD"}
+		abiInput := redeemStableCreditInput.ToAbiInput()
 
-//func TestRedeemStableCredit(t *testing.T) {
-//	t.Run("success", func(t *testing.T) {
-//		testHelper := testHelperWithMock(t)
-//		defer testHelper.MockController.Finish()
-//
-//		pricePerAssetUnit, _ := utils.StringToAmount("1")
-//
-//		input := &RedeemStableCreditInput{
-//			AssetCode: "vUSD",
-//		}
-//		abiInput := input.ToAbiInput()
-//
-//		testHelper.MockDRSContract.EXPECT().
-//			GetExchange(gomock.AssignableToTypeOf(&bind.CallOpts{}), abiInput.AssetCode).
-//			Return("vUSD", utils.StringToByte32("VELO"), pricePerAssetUnit, nil)
-//
-//		result, err := testHelper.Client.RedeemStableCredit(input)
-//
-//		assert.NoError(t, err)
-//		assert.NotNil(t, result)
-//		assert.Equal(t, "vUSD", result.AssetCode)
-//		assert.Equal(t, utils.StringToByte32("VELO"), result.CollateralAssetCode)
-//		assert.Equal(t, "10000000", result.PriceInCollateralPerAssetUnit.String())
-//	})
-//
-//	t.Run("fail, should throw error assetCode must not be blank", func(t *testing.T) {
-//		testHelper := testHelperWithMock(t)
-//		defer testHelper.MockController.Finish()
-//
-//		input := &RedeemStableCreditInput{
-//			AssetCode: "",
-//		}
-//
-//		result, err := testHelper.Client.RedeemStableCredit(input)
-//
-//		assert.NotNil(t, err)
-//		assert.Nil(t, result)
-//		assert.Equal(t, "assetCode must not be blank", err.Error())
-//	})
-//}
+		assert.NotNil(t, abiInput)
+	})
+}
+
+func TestRedeemStableCredit(t *testing.T) {
+	// TODO VELO-657
+}
