12092fcebed4d5d1eb859442bfdc438617088836
Jura Boonnom <jura@lightnet.io>
2020-02-06 15:13:04 +0700

    - fix var

--- go/cmd/gvel/constants/file.go
+++ go/cmd/gvel/constants/file.go
@@ -10,5 +10,5 @@ var FsBaseDir = path.Join(os.Getenv("HOME"), "/.gvel")
 var FsConfigFileNameFormat = path.Join(FsBaseDir, "%s-config.json")
 
 var FsSharedConfigFile = fmt.Sprintf(FsConfigFileNameFormat, "shared")
-var FsTestnetConfigFile = fmt.Sprintf(FsConfigFileNameFormat, EnvMainNet)
-var FsMainnetConfigFile = fmt.Sprintf(FsConfigFileNameFormat, EnvTestNet)
+var FsTestnetConfigFile = fmt.Sprintf(FsConfigFileNameFormat, EnvTestNet)
+var FsMainnetConfigFile = fmt.Sprintf(FsConfigFileNameFormat, EnvMainNet)
