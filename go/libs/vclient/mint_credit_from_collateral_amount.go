1fa6d07e56b7be07c104573ed8cca2bc6b1e56e6
WasinTaw <dreamwstt@gmail.com>
2020-02-11 11:17:19 +0700

    Replace error message

--- go/libs/vclient/mint_credit_from_collateral_amount.go
+++ go/libs/vclient/mint_credit_from_collateral_amount.go
@@ -102,6 +102,8 @@ func (c *Client) MintFromCollateralAmount(ctx context.Context, input *MintFromCo
 			return nil, errors.Errorf("the stable credit %s is not found", input.AssetCode)
 		case strings.Contains(msg, "transfer amount exceeds balance"):
 			return nil, errors.New("the collateral in your address is insufficient")
+		case strings.Contains(msg, "the stable credit does not belong to you"):
+			return nil, errors.Errorf("The stable credit %s does not belong to you", input.AssetCode)
 		default:
 			return nil, err
 		}
