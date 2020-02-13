671a2b8ce0a2aa8eaa08c8e6f0a596d9ca9cf24a
Michael <michael.ran@lightnet.io>
2020-02-13 19:23:34 +0800

    extract indexed field

--- go/libs/vclient/tx_helper.go
+++ go/libs/vclient/tx_helper.go
@@ -101,6 +101,7 @@ func (h *txHelper) ExtractRedeemEvent(eventName string, log *types.Log) (*vabi.D
 	if len(log.Topics) < 3 {
 		return nil, errors.New("fail to parse indexed param of an event")
 	}
-	event.CollateralAssetCode = utils.BytesToBytes32(log.Topics[1].Bytes())
+	event.CollateralAssetAddress = common.BytesToAddress(log.Topics[1].Bytes())
+	event.CollateralAssetCode = utils.BytesToBytes32(log.Topics[2].Bytes())
 	return event, nil
 }
