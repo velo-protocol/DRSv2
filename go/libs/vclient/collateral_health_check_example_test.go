4e364ba9a6edc1de6b159966a5404f7dfcf49640
WasinTaw <dreamwstt@gmail.com>
2020-02-17 16:41:41 +0700

    Update testkit

--- go/libs/vclient/collateral_health_check_example_test.go
+++ go/libs/vclient/collateral_health_check_example_test.go
@@ -21,10 +21,12 @@ func ExampleClient_CollateralHealthCheck() {
 	}
 
 	fmt.Println("CollateralAssetCode:", output[0].CollateralAssetCode)
+	fmt.Println("CollateralAssetAddress:", output[0].CollateralAssetAddress)
 	fmt.Println("RequiredAmount:", output[0].RequiredAmount)
 	fmt.Println("PresentAmount:", output[0].PresentAmount)
 	// Output:
 	// CollateralAssetCode: VELO
+	// CollateralAssetAddress: 0xbBa7C1C0222Eb4e323382FA3914fBC9E2dD58A9F
 	// RequiredAmount: 0.0000000
 	// PresentAmount: 0.0000000
 }
