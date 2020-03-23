0403878e7764376cc980fb8ad176855bf62ea5ea
WasinTaw <dreamwstt@gmail.com>
2020-03-23 16:47:44 +0700

    Add price migration

--- migrations/2_voracle_migration.js
+++ migrations/2_voracle_migration.js
@@ -4,6 +4,8 @@ const MedianizerProxy = artifacts.require('MedianizerProxy');
 const Medianizer = artifacts.require('Medianizer');
 const Lag = artifacts.require('Lag');
 const LagProxy = artifacts.require('LagProxy');
+const Price = artifacts.require('Price');
+const PriceProxy = artifacts.require('PriceProxy');
 
 module.exports = async (deployer, network, accounts) => {
   await deployer.deploy(FeederFactory);
@@ -106,7 +108,6 @@ module.exports = async (deployer, network, accounts) => {
     const initializeLagTHBCalldata = lagInstanceTHB.methods.initialize(accounts[0], medProxyTHB.address).encodeABI();
 
     await lagProxyTHB.initialize(lagLogic.address, initializeLagTHBCalldata);
-
     // Lag SGD
 
     await deployer.deploy(LagProxy, accounts[0]);
@@ -121,5 +122,41 @@ module.exports = async (deployer, network, accounts) => {
     console.log('lagProxyTHB', lagProxyTHB.address);
     console.log('lagProxySGD', lagProxySGD.address);
 
+    await deployer.deploy(Price);
+    const priceLogic = await Price.deployed();
+
+    // Price USD
+
+    await deployer.deploy(PriceProxy, accounts[0]);
+    const priceProxyUSD = await PriceProxy.deployed();
+
+    const priceInstanceUSD = new web3.eth.Contract(Price.abi, priceLogic.address);
+    const initializePriceUSDCalldata = priceInstanceUSD.methods.initialize(accounts[0], lagProxyUSD.address).encodeABI();
+
+    await priceProxyUSD.initialize(priceLogic.address, initializePriceUSDCalldata);
+
+    // Price THB
+
+    await deployer.deploy(PriceProxy, accounts[0]);
+    const priceProxyTHB = await PriceProxy.deployed();
+
+    const priceInstanceTHB = new web3.eth.Contract(Price.abi, priceLogic.address);
+    const initializePriceTHBCalldata = priceInstanceTHB.methods.initialize(accounts[0], lagProxyTHB.address).encodeABI();
+
+    await priceProxyTHB.initialize(priceLogic.address, initializePriceTHBCalldata);
+
+    // Price SGD
+
+    await deployer.deploy(PriceProxy, accounts[0]);
+    const priceProxySGD = await PriceProxy.deployed();
+
+    const priceInstanceSGD = new web3.eth.Contract(Price.abi, priceLogic.address);
+    const initializePriceSGDCalldata = priceInstanceSGD.methods.initialize(accounts[0], lagProxySGD.address).encodeABI();
+
+    await priceProxySGD.initialize(priceLogic.address, initializePriceSGDCalldata);
+
+    console.log('priceProxyUSD', priceProxyUSD.address);
+    console.log('priceProxyTHB', priceProxyTHB.address);
+    console.log('priceProxySGD', priceProxySGD.address);
   }
 };
