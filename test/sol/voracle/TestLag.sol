f42657c339cd9533369cd601d40e9ba73a9bf5cd
Jura Boonnom <jura@lightnet.io>
2020-03-23 14:52:49 +0700

    fix TestLag.sol

--- test/sol/voracle/TestLag.sol
+++ test/sol/voracle/TestLag.sol
@@ -13,24 +13,29 @@ contract TestLag {
     function beforeEach() public {
         mockMedianizer = new MockMedianizer(10);
         mockMedianizer2 = new MockMedianizer(11);
-        lag = new Lag(address(this), address(0));
+        lag = new Lag();
+        lag.initialize(address(this), address(0));
     }
 
     function testDeactivate() public {
-        lag.activate();
-        Assert.isTrue(lag.active(), "lag.active() should be true");
         lag.deactivate();
         Assert.isFalse(lag.active(), "lag.active() should be false");
     }
 
     function testDeactivateWhenNotGov() public {
-        lagNoneGov = new Lag(address(1), address(2));
+        lagNoneGov = new Lag();
+        lagNoneGov.initialize(address(1), address(2));
+
         (bool r,) = address(lagNoneGov).call(abi.encodePacked(lagNoneGov.deactivate.selector));
 
         Assert.equal(r, false, "lag.active() should be false");
     }
 
     function testActivate() public {
+        lag.setMedianizer(address(mockMedianizer));
+        lag.setMinimumPeriod(0);
+        lag.post();
+        lag.post();
         lag.deactivate();
         Assert.isFalse(lag.active(), "lag.active() should be false");
         lag.activate();
@@ -44,18 +49,18 @@ contract TestLag {
         Assert.equal(lag.medianizerAddr(), expectedMedianizer, "lag.setMedianizer() should be 0xb04aD816e86bFA5515c35Ad02081F71D9E848C88");
     }
 
-    function testSetLagTime() public {
-        bytes4 selector = lag.setLagTime.selector;
-        uint16 newLagTime = 60 * 60 * 16;
-        bytes memory data = abi.encodeWithSelector(selector, newLagTime);
+    function testSetMinimumPeriod() public {
+        bytes4 selector = lag.setMinimumPeriod.selector;
+        uint16 newMinPeriod = 60 * 60 * 16;
+        bytes memory data = abi.encodeWithSelector(selector, newMinPeriod);
         (bool r,) = address(lag).call(data);
 
-        Assert.isTrue(r, "lag.setLagTime() should not throw an error");
+        Assert.isTrue(r, "lag.setMinimumPeriod() should not throw an error");
     }
 
-    function testSetLagTimeWithError() public {
-        bytes4 selector = lag.setLagTime.selector;
-        uint16 newLagTime = 0;
+    function testSetMinimumPeriodWithError() public {
+        bytes4 selector = lag.setMinimumPeriod.selector;
+        uint256 newLagTime = 31 days;
         bytes memory data = abi.encodeWithSelector(selector, newLagTime);
         (bool r,) = address(lag).call(data);
 
@@ -66,27 +71,27 @@ contract TestLag {
         lag.void();
         Assert.equal(lag.active(), false, "lag.active() should be false after lag.void() has been called");
 
-        (uint256 price, bool isErr) = lag.getWithError();
+        (uint256 price, bool isActive, bool isErr) = lag.getWithError();
         Assert.equal(price, 0, "lag.getWithError() should return price equal to 0");
+        Assert.equal(isActive, false, "lag.getWithError() should return isActive equal to true");
         Assert.equal(isErr, true, "lag.getWithError() should return isErr equal to true");
-
     }
 
-    function testIsLagTimePass() public {
-        (bool r,) = address(lag).call(abi.encodePacked(lag.isLagTimePass.selector));
+    function testIsMinimumPeriodPass() public {
+        (bool r,) = address(lag).call(abi.encodePacked(lag.isMinimumPeriodPass.selector));
 
-        Assert.isTrue(r, "lag.isLagTimePass() should not throw an error");
+        Assert.isTrue(r, "lag.isMinimumPeriodPass() should not throw an error");
     }
 
     function testPost() public {
-        lag.activate();
         lag.setMedianizer(address(mockMedianizer));
-
+        lag.setMinimumPeriod(0);
         lag.post();
 
-        (uint256 curr, bool isErr) = lag.getWithError();
+        (uint256 price, bool isActive, bool isErr) = lag.getNextWithError();
 
-        Assert.equal(curr, 0, "curr should be 0");
+        Assert.equal(price, 10, "price should be 10");
+        Assert.equal(isActive, true, "isActive are should be false");
         Assert.equal(isErr, false, "isErr are should be false");
     }
 
@@ -104,32 +109,38 @@ contract TestLag {
     }
 
     function testGetWithError() public {
-        (uint256 curr, bool isErr) = lag.getWithError();
+        lag.setMedianizer(address(mockMedianizer));
+        lag.setMinimumPeriod(0);
+        lag.post();
+        lag.post();
 
-        Assert.equal(curr, 0, "lag.getWithError() curr should be 0");
+        (uint256 curr, bool isActive, bool isErr) = lag.getWithError();
+
+        Assert.equal(curr, 10, "lag.getWithError() curr should be 10");
+        Assert.isTrue(isActive, "lag.getWithError() isActive should be true");
         Assert.isFalse(isErr, "lag.getWithError() isErr should be false");
     }
 
     function testGetNextWithError() public {
-        (uint256 curr, bool isErr) = lag.getNextWithError();
+        lag.setMedianizer(address(mockMedianizer));
+        lag.setMinimumPeriod(0);
+        lag.post();
 
-        Assert.equal(curr, 0, "lag.getNextWithError() curr should be 0");
+        (uint256 curr, bool isActive, bool isErr) = lag.getNextWithError();
+
+        Assert.equal(curr, 10, "lag.getNextWithError() curr should be 10");
+        Assert.isTrue(isActive, "lag.getNextWithError() isActive should be true");
         Assert.isFalse(isErr, "lag.getNextWithError() isErr should be false");
     }
 
     function testGet() public {
-        lag.activate();
-        uint256 currPrice = lag.get();
-
-        Assert.equal(currPrice, 0, "lag.get() should be 0");
-    }
-
-    function testGetAfterPost() public {
-        lag.activate();
         lag.setMedianizer(address(mockMedianizer));
+        lag.setMinimumPeriod(0);
+        lag.post();
         lag.post();
 
         uint256 currPrice = lag.get();
-        Assert.equal(currPrice, 0, "lag.get() should be 0");
+
+        Assert.equal(currPrice, 10, "lag.get() should be 10");
     }
 }
